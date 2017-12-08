f0ea0d1bc05570c8a93580539a3909a968a7eabf
Dani <Dani@MSI-GL62M-7RD.mshome.net>
2017-12-08 21:26:28 +0000

    MVC implementation

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -1,10 +1,13 @@
 package moneycalculator;
 
+import moneycalculator.model.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
 import java.util.Scanner;
+import moneycalculator.model.ExchangeRate;
 
 public class MoneyCalculator {
 
@@ -13,11 +16,14 @@ public class MoneyCalculator {
         moneyCalculator.execute();
     }
 
-    private double amount;
-    private double exchangeRate;
-    private String currencyFrom;
-    private String currencyTo;
+    private final CurrencyList currencyList;
+    private Money money;
+    private ExchangeRate exchangeRate;
+    private Currency currencyTo;
 
+    public MoneyCalculator() {
+        this.currencyList = new CurrencyList();
+    }
 
     private void execute() throws Exception {
         input();
@@ -26,35 +32,52 @@ public class MoneyCalculator {
     }
 
     private void input() {
-        System.out.print("Introduzca una cantidad: ");
         Scanner scanner = new Scanner(System.in);
-        
-        amount = Double.parseDouble(scanner.next());
-        
-        System.out.print("Introduzca la divisa de origen: ");
-        currencyFrom = scanner.next();
-    
-        System.out.print("Introduzca la divisa de destino: ");
-        currencyTo = scanner.next();
+
+        System.out.print("Introduzca una cantidad: ");
+        double amount = 0;
+        while (true) {
+            try {
+                amount = Double.parseDouble(scanner.next());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.print("Introduzca una cantidad válida: ");
+            }
+        }
+
+        while (true) {
+            System.out.print("Introduzca el código de la divisa de origen: ");
+            Currency currency = currencyList.get(scanner.next());
+            money = new Money(amount, currency);
+            if (currency != null) break;
+            System.out.println("Divisa no conocida");
+        }
+
+        while (true) {
+            System.out.print("Introduzca el código de la divisa de destino: ");
+            currencyTo = currencyList.get(scanner.next());
+            if (currencyTo != null) break;
+            System.out.println("Divisa no conocida");
+        }
     }
 
     private void process() throws Exception {
-        exchangeRate = getExchangeRate(currencyFrom, currencyTo);
+        exchangeRate = getExchangeRate(money.getCurrency(), currencyTo);
     }
 
     private void output() {
-        System.out.println(amount + " " + currencyFrom + " equivalen a " + amount * exchangeRate + " " + currencyTo);
+        System.out.println(money.getAmount() + " " + money.getCurrency().getSymbol() + " equivalen a " + money.getAmount() * exchangeRate.getRate() + " " + currencyTo.getSymbol());
     }
 
-    private static double getExchangeRate(String from, String to) throws Exception {
-        URL url = new URL("http://api.fixer.io/latest?base=" + from + "&symbols=" + to);
+    private static ExchangeRate getExchangeRate(Currency from, Currency to) throws Exception {
+        URL url = new URL("http://api.fixer.io/latest?base=" + from.getCode() + "&symbols=" + to.getCode());
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         InputStreamReader input = new InputStreamReader(connection.getInputStream());
         try (BufferedReader reader = new BufferedReader(input)) {
             String line = reader.readLine();
-            line = line.substring(line.indexOf(to) + 5, line.indexOf("}"));
-            return Double.parseDouble(line);
+            line = line.substring(line.indexOf(to.getCode()) + 5, line.indexOf("}"));
+            return new ExchangeRate(from, to, new Date(), Double.parseDouble(line));
         }
     }
 }
